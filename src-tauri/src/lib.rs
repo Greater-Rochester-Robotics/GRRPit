0dd13090b479859c3c12b62cc3cad74568ef23c8
Bryce Roethel <me@bryceroethel.com>
2025-09-06 15:14:59 -0400

    First attempt

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -1,10 +1,24 @@
 use tauri::{WebviewUrl, WebviewWindowBuilder};
 
-#[cfg(target_os = "macos")]
-use {
-    tauri::TitleBarStyle,
-    window_vibrancy::{apply_vibrancy, NSVisualEffectMaterial}
-};
+#[tauri::command]
+fn get_macos_titlebar(webview_window: tauri::WebviewWindow) -> i32 {
+    #[cfg(target_os = "macos")]
+    {
+        use objc2_app_kit::NSWindow;
+        use objc2_foundation::NSRect;
+        use std::cmp::max;
+
+        let ns_window = webview_window.ns_window().unwrap();
+
+        let frame = ns_window.frame().unwrap();
+        let content = ns_window.contentRectForFrameRect(frame).unwrap();
+
+        let px: i32 = max(0, frame.size.height - content.size.height);
+        px.into()
+    }
+
+    (-1).into()
+}
 
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
 pub fn run() {
@@ -12,6 +26,7 @@ pub fn run() {
         .plugin(tauri_plugin_store::Builder::new().build())
         .plugin(tauri_plugin_opener::init())
         .plugin(tauri_plugin_http::init())
+        .invoke_handler(tauri::generate_handler![get_macos_titlebar])
         .setup(|app| {
             let win_builder =
                 WebviewWindowBuilder::new(app, "main", WebviewUrl::default())
@@ -20,17 +35,19 @@ pub fn run() {
                     .maximized(true);
 
             #[cfg(target_os = "macos")]
-            let win_builder = win_builder.title_bar_style(TitleBarStyle::Overlay);
+            {
+                use tauri::TitleBarStyle;
+                use window_vibrancy::{apply_vibrancy, NSVisualEffectMaterial};
 
-            let window = win_builder.build().unwrap();
-
-            #[cfg(target_os = "macos")]
-            apply_vibrancy(&window, NSVisualEffectMaterial::HudWindow, None, None)
-                .expect("Unsupported platform! 'apply_vibrancy' is only supported on macOS");
+                let window = win_builder.title_bar_style(TitleBarStyle::Overlay).build().unwrap();
+                apply_vibrancy(&window, NSVisualEffectMaterial::HudWindow, None, None)
+                    .expect("Unsupported platform! 'apply_vibrancy' is only supported on macOS");
+            }
 
             #[cfg(target_os = "windows")]
             {
                 use window_vibrancy::apply_mica;
+                let window = win_builder.build().unwrap();
                 apply_mica(&window, Some(true))
                     .expect("Unsupported platform! 'apply_mica' is only supported on Windows");
             }
