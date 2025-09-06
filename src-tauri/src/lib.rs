cc4bd53592ebe14af4d25f8a4c009c9dd8fc053a
Bryce Roethel <me@bryceroethel.com>
2025-09-06 15:44:19 -0400

    Try using AnyObject

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -1,13 +1,14 @@
-use tauri::{WebviewUrl, WebviewWindowBuilder};
+use tauri::{WebviewUrl, WebviewWindow, WebviewWindowBuilder};
 
 #[tauri::command]
-fn get_macos_titlebar(webview_window: tauri::WebviewWindow) -> f64 {
+fn get_macos_titlebar(webview_window: WebviewWindow) -> f64 {
     #[cfg(target_os = "macos")]
     {
+        use objc2::ffi::id;
         use objc2_app_kit::NSWindow;
         use objc2_foundation::NSRect;
 
-        let ns_window = webview_window.ns_window().unwrap();
+        let ns_window = webview_window.ns_window().unwrap() as id;
 
         let frame = ns_window.frame().unwrap();
         let content = ns_window.contentRectForFrameRect(frame).unwrap();
